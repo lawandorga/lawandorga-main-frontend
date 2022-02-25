c26fb51baf197b8a93b870daf6ad4878ca5b8e9d
Daniel Mössner <github@tortuga-webdesign.de>
2022-02-25 19:10:16 +0100

    notes

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -196,13 +196,6 @@ import useDeleteItem from "@/composables/useDeleteItem";
 import ModalForm from "@/components/ModalForm.vue";
 import ModalDelete from "@/components/ModalDelete.vue";
 
-const notes = [
-  {
-    title: "My first note",
-    note: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-  },
-];
-
 export default defineComponent({
   components: {
     ChevronRightIcon,
