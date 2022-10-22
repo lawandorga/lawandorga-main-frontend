362ff1d2a98967416686c8b09627bfdc64a068ec
Frederico Bormann <github@frederico.info>
2022-10-22 13:56:48 +0200

    Add breadcrumbs to EventsDashboard

--- src/views/events/EventsDashboard.vue
+++ src/views/events/EventsDashboard.vue
@@ -1,24 +1,38 @@
 <template>
-  <div v-if="actionsEvents">
-    <div class="flex justify-end">
-      <ButtonNormal
-        size="xs"
-        kind="action"
-        @click="actionsEvents.addEventModalOpen = true"
+  <BoxLoader :show="!!actionsEvents">
+    <div class="mx-auto space-y-6 max-w-screen-2xl">
+      <BreadcrumbsBar
+        class="lg:col-span-2"
+        :base="{ name: 'dashboard' }"
+        :pages="[]"
       >
-        Add Event
-      </ButtonNormal>
+        <CalendarDaysIcon class="w-6 h-6" />
+        <template #buttons>
+          <ButtonNormal
+            size="xs"
+            kind="action"
+            @click="actionsEvents.addEventModalOpen = true"
+          >
+            Add Event
+          </ButtonNormal>
+        </template>
+      </BreadcrumbsBar>
+      <div v-if="actionsEvents">
+        <div v-for="event in actionsEvents.events" :key="event.id">
+          {{ event.name }} {{ event.start_time }} {{ event.end_time }}
+        </div>
+      </div>
     </div>
-    <div v-for="event in actionsEvents.events" :key="event.id">
-      {{ event.name }} {{ event.start_time }} {{ event.end_time }}
-    </div>
-  </div>
+  </BoxLoader>
   <ActionsEvents ref="actionsEvents" />
 </template>
 
 <script setup lang="ts">
 import ActionsEvents from "@/components/ActionsEvents.vue";
 import { ButtonNormal } from "@lawandorga/components";
+import { CalendarDaysIcon } from "@heroicons/vue/24/outline";
+import BreadcrumbsBar from "@/components/BreadcrumbsBar.vue";
+import BoxLoader from "@/components/BoxLoader.vue";
 import { ref } from "vue";
 
 const actionsEvents = ref<typeof ActionsEvents>();
