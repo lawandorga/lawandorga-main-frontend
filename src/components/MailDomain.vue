9f804b4117f33138ec91886f4893a7be3c860b08
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-01 14:22:57 +0100

    update

--- src/components/MailDomain.vue
+++ src/components/MailDomain.vue
@@ -70,8 +70,8 @@
               </tr>
               <tr>
                 <td>CNAME (DKIM)</td>
-                <td>2022-12._domainkey.{{ domain.name }}</td>
-                <td>2022-12.dkim.mail.law-orga.de.</td>
+                <td>2022-12.law-orga._domainkey.{{ domain.name }}</td>
+                <td>2022-12.law-orga.dkim.mail.law-orga.de.</td>
                 <td>3600</td>
                 <td>-</td>
               </tr>
