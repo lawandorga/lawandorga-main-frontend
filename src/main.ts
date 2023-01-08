1ae082a3769ecbd6e1846a894491caa52ee583a4
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-08 16:48:07 +0100

    add environment

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ if (import.meta.env.PROD) {
         tracePropagationTargets: ["localhost", "law-orga.de", /^\//],
       }),
     ],
+    environment: "production",
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
