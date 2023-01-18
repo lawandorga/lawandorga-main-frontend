869958fab03b97715bbb3bb3444147c387f4352e
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-18 18:08:38 +0100

    change coimments

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import dns from "dns";
 
-// https://vitejs.dev/config/
+// sets localhost as default instead of 127.0.0.1
 dns.setDefaultResultOrder("verbatim");
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
