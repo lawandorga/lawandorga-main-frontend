25d9079bcd879936275f10b8df24b0b02d7d4283
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-26 18:16:09 +0100

    improve download

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -6,6 +6,7 @@ export function createObjectURL(data: Blob): string {
   } else if (window.URL && window.URL.createObjectURL) {
     return window.URL.createObjectURL(data);
   } else {
+    alert("Error downloading file.");
     throw Error("No object url could be created.");
   }
 }
@@ -18,16 +19,8 @@ export function downloadFile(
   const filename: string = name;
   const objectUrl = createObjectURL(response.data);
 
-  if (name.split(".").pop() === "pdf") {
-    if (openedWindow) openedWindow.location = objectUrl;
-    else {
-      const downloadLink = document.createElement("a");
-      downloadLink.setAttribute("target", "_blank");
-      downloadLink.href = objectUrl;
-      downloadLink.setAttribute("download", filename);
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-    }
+  if (name.split(".").pop() === "pdf" && openedWindow) {
+    openedWindow.location = objectUrl;
   } else {
     if (openedWindow) openedWindow.close();
     const downloadLink = document.createElement("a");
