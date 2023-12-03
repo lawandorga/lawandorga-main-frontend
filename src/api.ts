d24d2171b3078b076066d1b58db95519861d00d3
Daniel Mössner <github@moesibert.de>
2023-12-03 22:13:27 +0100

    update some packages

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import axios, { AxiosInstance } from "axios";
 export function setupDefaultAxios($axios: AxiosInstance) {
   $axios.defaults.baseURL = import.meta.env.VITE_API_URL as string;
   $axios.defaults.withCredentials = true;
+  $axios.defaults.withXSRFToken = true;
   $axios.defaults.xsrfHeaderName = "x-csrftoken";
   $axios.defaults.xsrfCookieName = "csrftoken";
 
