6264bf27e19c9ddc37b0e4a98a6196c1c5205a69
Daniel Mössner <github@moesibert.de>
2024-02-20 12:59:48 +0100

    sceleton

---
+++ src/features/mail_imports/views/FolderMailImports.vue
@@ -0,0 +1,5 @@
+<!-- similar to FolderDataSheet.vue for example  -->
+<!-- will be used in features/folders/views/FoldersDetail.vue -->
+<script setup lang="ts"></script>
+
+<template><div>todo</div></template>
