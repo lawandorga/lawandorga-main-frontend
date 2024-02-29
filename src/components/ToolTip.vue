507e85a32a9662b162df41a1c0191e8dac2f1c4c
LeandraH <leandravhahn@gmail.com>
2024-02-29 18:34:07 +0100

    Enable clicks through a Tooltip

--- src/components/ToolTip.vue
+++ src/components/ToolTip.vue
@@ -2,7 +2,7 @@
   <div class="relative self-center has-tooltip w-min">
     <div
       role="tooltip"
-      class="invisible absolute px-4 py-2 transform -translate-x-1/2 rounded tooltip bg-neutral-500/90 w-max bottom-7 left-1/2 after:content-[''] after:absolute after:ml-[-5px] after:border-[5px] after:border-[rgb(115_115_115_/_0.9)_transparent_transparent_transparent] after:left-2/4 after:top-full"
+      class="invisible absolute px-4 py-2 transform -translate-x-1/2 pointer-events-none rounded tooltip bg-neutral-500/90 w-max bottom-7 left-1/2 after:content-[''] after:absolute after:ml-[-5px] after:border-[5px] after:border-[rgb(115_115_115_/_0.9)_transparent_transparent_transparent] after:left-2/4 after:top-full"
     >
       {{ text }}
     </div>
