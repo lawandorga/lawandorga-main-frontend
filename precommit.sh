c00f7242d7b32af84fc2db4f7b5875fb17f54b9e
LeandraH <leandravhahn@gmail.com>
2024-03-11 12:55:14 +0100

    Add tscheck back into precommit

--- precommit.sh
+++ precommit.sh
@@ -2,7 +2,7 @@
 
 printf "\nlint:\n" && npm run lint
 printf "\n\n"
-# printf "typescript:\n" && npm run tscheck
-# printf "\n\n"
+printf "typescript:\n" && npm run tscheck
+printf "\n\n"
 # printf "tests:\n" && npm run test
 # printf "\n\n"
