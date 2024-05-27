20ffaed06a960dcc4f454410e3822747381a8b64
LeandraH <leandravhahn@gmail.com>
2024-05-27 19:03:09 +0200

    Fix lint

--- src/features/admin/views/AdminCollabDocuments.vue
+++ src/features/admin/views/AdminCollabDocuments.vue
@@ -3,7 +3,6 @@ import BreadcrumbsBar from "@/components/BreadcrumbsBar.vue";
 import { CogIcon } from "@heroicons/vue/24/outline";
 import useCollab from "../../records/api/useCollab";
 import { ButtonNormal, ModalCreate } from "lorga-ui";
-import { PlusCircleIcon } from "@heroicons/vue/24/outline";
 import DeleteCollabTemplate from "../actions/DeleteCollabTemplate.vue";
 import CreateCollabLetterhead from "../actions/CreateCollabLetterhead.vue";
 
