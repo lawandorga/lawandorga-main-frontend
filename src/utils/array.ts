f7e8231c1023c08636b2ac2b9213f1a97d6c16ff
Daniel Mössner <github@moesibert.de>
2024-06-01 20:24:20 +0200

    refactor

--- src/utils/array.ts
+++
@@ -1,9 +0,0 @@
-import { DjangoModel } from "@/types/shared";
-
-export const removeFromArray = (
-  array: DjangoModel[],
-  id: number,
-): DjangoModel[] => {
-  const newArray = array.filter((item) => item.id !== id);
-  return newArray;
-};
