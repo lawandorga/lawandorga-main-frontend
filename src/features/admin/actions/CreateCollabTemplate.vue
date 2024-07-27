bde6a3c879db29a53f5c3d03b56097e88aa1e350
LeandraH <leandravhahn@gmail.com>
2024-07-27 09:55:58 +0200

    Create uuid in frontend

--- src/features/admin/actions/CreateCollabTemplate.vue
+++ src/features/admin/actions/CreateCollabTemplate.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import { ButtonNormal, ModalForm, types } from "lorga-ui";
-import { computed, toRefs } from "vue";
+import { computed, ref, toRefs } from "vue";
 import useCmd from "@/composables/useCmd";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "vue-router";
 
 const props = defineProps<{
   query: () => void;
 }>();
 const { query } = toRefs(props);
-
 const { commandRequest, commandModalOpen } = useCmd(query.value);
+
+const uuid = ref(uuidv4());
+const router = useRouter();
 
 const fields = computed<types.FormField[]>(() => [
   {
@@ -33,9 +37,16 @@ const fields = computed<types.FormField[]>(() => [
       v-model="commandModalOpen"
       title="Create Template"
       :fields="fields"
-      :request="commandRequest"
+      :request="
+        async (props: any) => {
+          await commandRequest(props);
+          router.push({
+            path: `/admin/collab/template/${uuid}`,
+          });
+        }
+      "
       submit="Create"
-      :data="{ action: 'collab/create_template' }"
+      :data="{ action: 'collab/create_template', uuid: uuid }"
     />
   </ButtonNormal>
 </template>
