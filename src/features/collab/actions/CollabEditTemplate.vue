657cc2f7c37032f5bc99e8edcd2df680f2a62d0b
LeandraH <leandravhahn@gmail.com>
2024-07-27 15:13:28 +0200

    Improve template editing screen

--- src/features/collab/actions/CollabEditTemplate.vue
+++ src/features/collab/actions/CollabEditTemplate.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
-import { ButtonNormal, ModalForm, types } from "lorga-ui";
+import {
+  ButtonNormal,
+  ModalFree,
+  TableBody,
+  TableData,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableTable,
+} from "lorga-ui";
 import { ArrowPathIcon } from "@heroicons/vue/24/outline";
-import { computed, toRefs } from "vue";
+import { ref, toRefs } from "vue";
 import useCollabTemplates from "@/features/admin/api/useCollabTemplates";
 import useCmd from "@/composables/useCmd";
 import { CollabTemplate } from "@/features/admin/api/useTemplate";
@@ -12,25 +21,12 @@ const props = defineProps<{
   uuid: string;
   selectedTemplate: CollabTemplate | null;
 }>();
-const { query } = toRefs(props);
+const { query, selectedTemplate } = toRefs(props);
 
 const { commandRequest, commandModalOpen } = useCmd(query.value);
 
 const { templates } = useCollabTemplates();
-
-const fields = computed<types.FormField[]>(() => [
-  {
-    label: "Template",
-    name: "template_uuid",
-    type: "select",
-    options: templates.value,
-    required: false,
-  },
-  {
-    name: "preview",
-    type: "slot",
-  },
-]);
+const selectedTemplateId = ref<string>(selectedTemplate.value?.uuid ?? "");
 </script>
 
 <template>
@@ -38,20 +34,55 @@ const fields = computed<types.FormField[]>(() => [
     <ArrowPathIcon class="w-5 h-5 mr-2" />
     Change Template
   </ButtonNormal>
-  <ModalForm
+  <ModalFree
     v-model="commandModalOpen"
     title="Change Template"
-    :fields="fields"
-    submit="Change template"
-    :request="commandRequest"
-    :data="{
-      action: 'collab/assign_template_to_collab',
-      collab_uuid: uuid,
-      template_uuid: selectedTemplate?.uuid,
-    }"
+    width="max-w-4xl"
   >
-    <template #preview="{ data }">
-      <TemplatePreview :data="data" />
-    </template>
-  </ModalForm>
+    <TableTable>
+      <TableHead>
+        <TableRow>
+          <TableHeader><span class="ml-[27px]">Name</span></TableHeader>
+          <TableHeader>Description</TableHeader>
+          <TableHeader />
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow v-for="template in templates" :key="template.uuid">
+          <label :for="template.uuid" class="contents">
+            <TableData>
+              <input
+                :id="template.uuid"
+                v-model="selectedTemplateId"
+                type="radio"
+                :value="template.uuid"
+                class="mr-2"
+              />
+              {{ template.name }}
+            </TableData>
+            <TableData>
+              {{ template.description }}
+            </TableData>
+            <TableData>
+              <TemplatePreview :data="{ template_uuid: template.uuid }" />
+            </TableData>
+          </label>
+        </TableRow>
+      </TableBody>
+    </TableTable>
+    <ButtonNormal
+      class="flex justify-end mt-4"
+      @click="
+        () => {
+          commandRequest({
+            action: 'collab/assign_template_to_collab',
+            collab_uuid: uuid,
+            template_uuid: selectedTemplateId,
+          });
+        }
+      "
+    >
+      Change template
+    </ButtonNormal>
+  </ModalFree>
 </template>
