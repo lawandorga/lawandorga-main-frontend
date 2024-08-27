fe0a753be82277ce7e4d8c6e0dc6027d4787c639
LeandraH <leandravhahn@gmail.com>
2024-08-27 12:40:15 +0200

    Another nicer button

--- src/components/ButtonBreadcrumbs.vue
+++ src/components/ButtonBreadcrumbs.vue
@@ -1,12 +1,7 @@
 <template>
-  <component
+  <ButtonNormal
     :is="is"
-    class="inline-flex items-center text-center px-6 min-h-[2.75rem] border border-transparent text-base font-medium rounded-md shadow-sm text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:ring-0 focus:bg-gray-50"
-    :class="[
-      disabledComputed
-        ? 'pointer-events-none opacity-80 cursor-not-allowed'
-        : '',
-    ]"
+    kind="secondary"
     :to="to"
     :href="href"
     :disabled="disabledComputed"
@@ -15,17 +10,18 @@
   >
     <CircleLoader v-show="loading" color="text-white" class="mr-2" />
     <slot />
-  </component>
+  </ButtonNormal>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { CircleLoader } from "lorga-ui";
+import { ButtonNormal, CircleLoader } from "lorga-ui";
 
 export default defineComponent({
   components: {
     CircleLoader,
-  },
+    ButtonNormal
+},
   props: {
     to: {
       type: [String, Object],
