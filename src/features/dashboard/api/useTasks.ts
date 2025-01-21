918969454af8737b3d687212b2502272b5c5e1a9
JoeyStk <johannes.stock@yahoo.de>
2025-01-21 15:20:01 +0100

    More work from 01-14

--- src/features/dashboard/api/useTasks.ts
+++ src/features/dashboard/api/useTasks.ts
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
-import { Ref, ref, watch } from "vue";
-=======
 import useGet from "@/composables/useGet";
 import useGet2 from "@/composables/useGet2";
 import { ref } from "vue";
->>>>>>> Stashed changes
 import useClient from "@/api/client";
 import useGet2 from "@/composables/useGet2";
 import useQuery from "@/composables/useQuery";
@@ -20,27 +16,6 @@ interface Task {
   created_at: string;
 }
 
-<<<<<<< Updated upstream
-export const useTasks = (id: Ref<number | undefined>) => {
-  const assigned = useAssignedTasks(id);
-  const created = useCreatedTasks();
-
-  return {
-    assignedTasks: assigned.tasks,
-    createdTasks: created.tasks,
-  };
-};
-
-const useAssignedTasks = (id: Ref<number | undefined>) => {
-  const client = useClient();
-  const tasks = ref<Task[]>();
-  const query = useQuery(client.get("api/tasks/query/{}/own/", id), tasks);
-
-  watch(id, (newValue) => {
-    if (newValue) query();
-  });
-
-=======
 export const useTasks = () => {
   return {
     assignedTasks: useAssignedTasks(),
@@ -54,7 +29,6 @@ const useAssignedTasks = () => {
 
   useGet(client.get(`api/tasks/query/own/`), tasks);
   console.log("Tasks:", tasks.value);
->>>>>>> Stashed changes
   return {
     tasks,
   };
@@ -63,17 +37,10 @@ const useAssignedTasks = () => {
 const useCreatedTasks = () => {
   const tasks = ref<Task[] | null>(null);
 
-<<<<<<< Updated upstream
-  const createdTodosQuery = useGet2(`api/tasks/query/created/`, tasks);
-
-  return {
-    createdTodosQuery,
-=======
   const createdTaskQuery = useGet2(`api/tasks/query/created/`, tasks);
   console.log("created tasks:", tasks.value);
   return {
     createdTaskQuery,
->>>>>>> Stashed changes
     tasks,
   };
 };
