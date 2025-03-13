d0d64917bfd31467dd536b105d5a5fe92dd19919
LeandraH <LeandraH@users.noreply.github.com>
2025-03-13 10:42:31 +0100

    Fix lint

--- src/api/errors.ts
+++ src/api/errors.ts
@@ -255,7 +255,7 @@ export function cleanUpError(context: Context): ICommandError {
       newError.paramErrors = data;
       newError.title = "Request Error";
     }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+    // eslint-disable-next-line no-unused-vars
   } catch (_) {
     newError.title = "Unknown Error";
     if (error.response?.status)
--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,6 +1,6 @@
 declare module "*.vue" {
   import { DefineComponent } from "vue";
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-object-type
-  const component: DefineComponent<{}, {}, any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<object, object, any>;
   export default component;
 }
