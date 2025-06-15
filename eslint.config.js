57217d0cc10035f9920868c9815a92754785d3f7
LeandraH <LeandraH@users.noreply.github.com>
2025-06-15 14:46:49 +0200

    Fix config

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,7 @@ export default [
   ...eslintrc.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:vue/vue3-recommended",
+    "plugin:vue/recommended",
     "prettier",
   ),
   ...eslintrc.plugins("prettier"),
