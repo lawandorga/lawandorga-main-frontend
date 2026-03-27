8e2ea1f715433aa84bc30a36be5bc2f9705c2573
JoeyStk <johannes.stock@yahoo.de>
2026-03-27 10:46:28 +0100

    WiP

--- src/features/dashboard/api/useArticles.ts
+++ src/features/dashboard/api/useArticles.ts
@@ -1,10 +1,12 @@
 import useGet2 from "@/composables/useGet2";
+import type { OrgUser } from "@/features/admin/api/useProfileData";
 import { ref } from "vue";
 
 interface Article {
   id: number;
   title: string;
   description: string;
+  recipients: OrgUser[];
   date: Date;
 }
 
