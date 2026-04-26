b06a9a2351ed3c726c8e454489e645328533e1cc
JoeyStk <johannes.stock@yahoo.de>
2026-04-26 22:35:43 +0200

    Revert changes

--- src/features/dashboard/api/useArticles.ts
+++ src/features/dashboard/api/useArticles.ts
@@ -1,14 +1,10 @@
 import useGet2 from "@/composables/useGet2";
-import type { OrgUser } from "@/features/admin/api/useProfileData";
-import { useUserStore } from "@/store/user";
-import { storeToRefs } from "pinia";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 interface Article {
   id: number;
   title: string;
   description: string;
-  recipients: OrgUser[];
   date: Date;
 }
 
@@ -16,16 +12,7 @@ export function useArticles() {
   const articles = ref<Article[] | null>();
   useGet2("/api/internal/articles/dashboard/", articles);
 
-  const { user } = storeToRefs(useUserStore());
-
-  const filteredArticles = computed(() => {
-    if (!articles.value || !user.value) return null;
-    return articles.value.filter((article) =>
-      article.recipients.some((recipient) => recipient.id === user.value!.id),
-    );
-  });
-
   return {
-    articles: filteredArticles,
+    articles,
   };
 }
