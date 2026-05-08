9ae9c699512745ea7f71ef4891fdda2f1f67de09
JoeyStk <johannes.stock@yahoo.de>
2026-05-08 17:31:22 +0200

    Refactor NavigationSidebar

--- src/components/NavigationSidebar.vue
+++ src/components/NavigationSidebar.vue
@@ -9,7 +9,7 @@ import { RouteLocationRaw, useRoute, useRouter } from "vue-router";
 import LogoWhite from "./LogoWhite.vue";
 import { CircleLoader } from "lorga-ui";
 import { storeToRefs } from "pinia";
-import RedBadge from "./RedBadge.vue";
+import NavigationSidebarItem from "./NavigationSidebarItem.vue";
 
 const { navigationItems } = useNavigationItems();
 const userStore = useUserStore();
@@ -17,10 +17,12 @@ const { loaded } = storeToRefs(userStore);
 const route = useRoute();
 const router = useRouter();
 
-const expanded = ref(userStore.getSetting("navigationExpanded", true));
+const expanded = ref(
+  userStore.getSetting("navigationExpanded", true) as boolean,
+);
 
 watch(loaded, () => {
-  expanded.value = userStore.getSetting("navigationExpanded", true);
+  expanded.value = userStore.getSetting("navigationExpanded", true) as boolean;
 });
 
 const expandClicked = () => {
@@ -62,28 +64,24 @@ const isNavigationItemActive = (item: NavigationItem): boolean => {
           </button>
         </div>
         <div
-          v-show="expanded"
-          class="flex items-center px-4 h-14 border-white/20"
+          class="flex items-center h-14 border-white/20"
+          :class="{ 'px-4': expanded, 'border-b': !expanded }"
         >
           <router-link
             :to="{ name: 'start' }"
-            class="flex items-center h-10 px-4 -ml-2 space-x-2 rounded hover:bg-gray-50/10"
+            class="flex items-center rounded hover:bg-gray-50/10"
+            :class="
+              expanded
+                ? 'h-10 px-4 -ml-2 space-x-2'
+                : 'justify-center w-10 h-10 mx-auto'
+            "
           >
-            <div class="-ml-2">
+            <div :class="{ '-ml-2': expanded }">
               <LogoWhite />
             </div>
-            <h1 class="text-2xl font-bold text-white">Law&Orga</h1>
-          </router-link>
-        </div>
-        <div
-          v-show="!expanded"
-          class="flex items-center border-b h-14 border-white/20"
-        >
-          <router-link
-            :to="{ name: 'start' }"
-            class="flex items-center justify-center w-10 h-10 mx-auto rounded hover:bg-gray-50/10"
-          >
-            <LogoWhite />
+            <h1 v-show="expanded" class="text-2xl font-bold text-white">
+              Law&Orga
+            </h1>
           </router-link>
         </div>
         <div
@@ -107,67 +105,13 @@ const isNavigationItemActive = (item: NavigationItem): boolean => {
               'space-y-1 px-2 pt-2': expanded,
             }"
           >
-            <template v-for="item in navigationItems" :key="item.label">
-              <div v-if="item.divider">
-                <div
-                  class=""
-                  :class="{
-                    'w-full mt-4': expanded,
-                    'w-10 py-0.5 mx-auto': !expanded,
-                  }"
-                >
-                  <div
-                    class="w-full pt-2 border-t"
-                    :class="{
-                      'border-white/20': expanded,
-                      'border-white/50': !expanded,
-                    }"
-                  >
-                    <span class="block px-2 text-xs uppercase text-white/50"
-                      >External Links</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <component
-                :is="item.is"
-                v-else
-                v-bind="item.attrs"
-                class="relative flex items-center justify-between py-2 pl-2 text-sm font-medium rounded-md group text-white/70 hover:bg-white/10 hover:text-white"
-                :class="{
-                  'text-gray-700 bg-white/10 hover:bg-white/10':
-                    isNavigationItemActive(item),
-                  'pb-5.5!': !expanded && item.is === 'a',
-                  'w-10 pr-2 mx-auto': !expanded,
-                  'pr-3': expanded,
-                }"
-              >
-                <div class="flex items-center">
-                  <component
-                    :is="item.icon"
-                    class="w-6 h-6 text-white/60 shrink-0 group-hover:text-white"
-                    :class="{ 'mr-3': expanded }"
-                  />
-                  <span
-                    v-show="expanded || item.is === 'a'"
-                    :class="{
-                      'text-xs absolute block bold top-8 truncate left-0 right-0 text-center text-gray-500':
-                        !expanded,
-                    }"
-                  >
-                    {{ item.label }}
-                  </span>
-                </div>
-                <RedBadge
-                  v-if="item.notifications && item.notifications > 0"
-                  :additional-classes="
-                    expanded ? '' : 'absolute top-1 right-1 w-4! h-4! text-xs!'
-                  "
-                >
-                  {{ item.notifications }}
-                </RedBadge>
-              </component>
-            </template>
+            <NavigationSidebarItem
+              v-for="item in navigationItems"
+              :key="item.label"
+              :item="item"
+              :expanded="expanded"
+              :active="isNavigationItemActive(item)"
+            />
           </nav>
           <div
             v-show="expanded"
