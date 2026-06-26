b7f2e3b8b69c251ce2d99a4ff1f1fb3530ae47f7
JoeyStk <johannes.stock@yahoo.de>
2026-06-26 22:03:00 +0200

    Fix article styling

--- src/features/dashboard/components/ArticleList.vue
+++ src/features/dashboard/components/ArticleList.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { CalendarIcon, ArrowRightIcon } from "@heroicons/vue/24/outline";
+
 import BoxLoader from "@/components/BoxLoader.vue";
 import BoxSection from "@/components/BoxSection.vue";
+
 import { useArticles } from "../api/useArticles";
-import { CalendarIcon, ArrowRightIcon } from "@heroicons/vue/24/outline";
 const { articles } = useArticles();
 </script>
 
@@ -11,33 +13,33 @@ const { articles } = useArticles();
     <BoxLoader :show="!!articles" class="px-6 py-4">
       <div
         v-if="articles?.length"
-        class="grid grid-cols-1 gap-6 px-6 py-4 mt-2 lg:grid-cols-3"
+        class="mt-2 grid grid-cols-1 gap-6 px-6 py-4 lg:grid-cols-3"
       >
         <article
           v-for="article in articles"
           :key="article.id"
-          class="flex flex-col justify-between px-6 py-4 bg-white border border-gray-200 rounded hover:border-formcolor/20 hover:shadow-md"
+          class="hover:border-formcolor/20 flex flex-col justify-between rounded border border-gray-200 bg-white px-6 py-4 hover:shadow-md"
         >
-          <div class="flex items-center mb-2 text-sm text-gray-500">
-            <CalendarIcon class="w-5 h-5 mr-1"></CalendarIcon>
+          <div class="mb-2 flex items-center text-xs text-gray-500">
+            <CalendarIcon class="mr-1 h-3 w-3"></CalendarIcon>
             <span>
               {{ new Date(article.date).toLocaleDateString() }}
             </span>
           </div>
-          <div>
-            <h3 class="mb-2 font-medium text-gray-700">
+          <div class="mb-4">
+            <h3 class="mb-3 font-medium text-[18px]">
               {{ article.title }}
             </h3>
             <p
-              class="text-sm text-gray-700 wrap-break-word whitespace-pre-line [&>a]:font-medium [&>a]:text-formcolor"
+              class="text-sm wrap-break-word whitespace-pre-line text-muted"
             >
               {{ article.preview }}
             </p>
           </div>
           <router-link
             :to="{ name: 'internal-article', params: { id: article.id } }"
-            class="mt-2"
-            >Read more <ArrowRightIcon class="w-4 h-4 ml-2 text-gray-400" />
+            class="text-formcolor dark:hover:bg-accent/50 inline-flex self-start items-center gap-1.5 px-2.5 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none"
+            >Read more <ArrowRightIcon class="ml-2 h-3 w-3 text-gray-400" />
           </router-link>
         </article>
       </div>
