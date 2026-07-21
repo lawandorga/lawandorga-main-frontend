9f1decd8987a3b0735330e7fb50f23538e80b3b0
danielmoessner <github@danielmoessner.de>
2026-07-21 15:26:55 +0200

    fix: formatting

--- src/features/calendar/components/CalendarFiltersPanel.vue
+++ src/features/calendar/components/CalendarFiltersPanel.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { ButtonNormal } from "lorga-ui";
+import { computed } from "vue";
 
 import type { CalendarEvent } from "../api/useCalendarEvents";
 import {
