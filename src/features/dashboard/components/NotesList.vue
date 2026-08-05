8581edac8ba73d4af77f97ac6ba70a3ad4940d75
Johannes <72705147+JoeyStk@users.noreply.github.com>
2026-08-05 14:24:19 +0200

    656: Add created and updated to notes (#658)

--- src/features/dashboard/components/NotesList.vue
+++ src/features/dashboard/components/NotesList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { CalendarIcon } from "@heroicons/vue/24/outline";
 import { computed } from "vue";
 
 import BoxLoader from "@/components/BoxLoader.vue";
@@ -8,6 +9,7 @@ import DeleteNote from "@/features/dashboard/actions/DeleteNote.vue";
 import UpdateNote from "@/features/dashboard/actions/UpdateNote.vue";
 import { useNotes } from "@/features/dashboard/api/useNotes";
 import { useUserStore } from "@/store/user";
+import { formatDate } from "@/utils/date";
 
 const userStore = useUserStore();
 const { notes, notesQuery } = useNotes();
@@ -30,6 +32,13 @@ const canManageNotes = computed(() =>
           class="hover:border-formcolor/20 rounded-lg border border-gray-200 bg-white p-4 transition-all duration-200 hover:shadow-md"
           :class="{ 'lg:col-span-2': note.is_wide }"
         >
+          <div class="mb-2 flex items-center text-xs text-gray-500">
+            <CalendarIcon class="mr-1 h-3 w-3" />
+            <span>Created: {{ formatDate(note.created) }}</span>
+            <span v-if="note.updated"
+              >, updated {{ formatDate(note.updated) }}</span
+            >
+          </div>
           <div class="flex justify-between">
             <h3 class="mb-2 flex items-center gap-2 font-medium text-gray-700">
               {{ note.title }}
